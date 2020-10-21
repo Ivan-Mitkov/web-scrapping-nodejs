169742afcc3153becf9da2d6128cae2dcbc6cef9
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-10-21 16:47:32 +0300

    select attributes

--- intro/index.js
+++ intro/index.js
@@ -23,5 +23,14 @@ async function multipleEls() {
   });
   fs.writeFileSync("./test.html", text.join("\n"));
 }
-
-multipleEls();
+//select with attribute
+async function selectAttr() {
+  const html = await request.get(
+    "https://reactnativetutorial.net/css-selectors/lesson6.html"
+  );
+  // fs.writeFileSync("./test.html", html);
+  const $ = await cheerio.load(html);
+  const text = $('[data-customer="22293"]').text();
+  fs.writeFileSync("./test2.html", text);
+}
+selectAttr();
