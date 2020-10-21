d3f118a57e22d26028850ea7e8f83c4059bfdc55
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-10-21 15:50:25 +0300

    the simplest scrapper

--- intro/index.js
+++ intro/index.js
@@ -1,10 +1,14 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
-
+const fs = require("fs");
 async function main() {
   const html = await request.get(
     "https://reactnativetutorial.net/css-selectors/"
   );
+  // fs.writeFileSync("./test.html", html);
+  const $ = await cheerio.load(html);
+  const text = $("h1").text();
+  fs.writeFileSync("./test.html", text);
 }
 
 main();
