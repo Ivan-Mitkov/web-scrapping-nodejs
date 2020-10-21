183f7bf8cca2c7e9b7a63d618b69c5ae9eedef25
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-10-21 16:36:52 +0300

    multiple elements

--- intro/index.js
+++ intro/index.js
@@ -10,5 +10,18 @@ async function main() {
   const text = $("h1").text();
   fs.writeFileSync("./test.html", text);
 }
+async function multipleEls() {
+  const html = await request.get(
+    "https://reactnativetutorial.net/css-selectors/lesson2.html"
+  );
+  // fs.writeFileSync("./test.html", html);
+  const $ = await cheerio.load(html);
+  const text = [];
+  $("h2").each((index, el) => {
+    const textEl = $(el).text();
+    text.push(textEl);
+  });
+  fs.writeFileSync("./test.html", text.join("\n"));
+}
 
-main();
+multipleEls();
